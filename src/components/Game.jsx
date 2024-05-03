d6002bc83313e8c3ce55920fbff438cf92f1e4f9
HseongH <hh4518@gmail.com>
2024-05-04 01:25:43 +0900

    동작을 오름차순 또는 내림차순으로 정렬할 수 있는 토글 버튼을 추가해 보세요.

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -4,9 +4,13 @@ import Board from "./Board";
 export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
+  const [orderBy, setOrderBy] = useState("asc");
   const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
 
+  const handleOrderBy = () => {
+    setOrderBy(orderBy === "asc" ? "desc" : "asc");
+  };
   const handlePlay = (nextSquares) => {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
     setHistory(nextHistory);
@@ -17,27 +21,34 @@ export default function Game() {
     setCurrentMove(nextMove);
   };
 
+  const moves = history.map((squares, move) => {
+    const description = move > 0 ? `Go to move #${move}` : "Go to game start";
+
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
   return (
     <div className="game">
       <div className="game-board">
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
-
-        <div className="status">{`당신은 ${currentMove}번째 순서에 있습니다…`}</div>
       </div>
 
       <div className="game-info">
-        <ol>
-          {history.map((squares, move) => {
-            const description =
-              move > 0 ? `Go to move #${move}` : "Go to game start";
+        <button onClick={handleOrderBy}>{orderBy}</button>
 
-            return (
-              <li key={move}>
-                <button onClick={() => jumpTo(move)}>{description}</button>
-              </li>
-            );
-          })}
+        <ol reversed={orderBy === "desc"}>
+          {orderBy === "asc" ? moves : moves.reverse()}
         </ol>
+
+        <div className="status">
+          {currentMove > 0
+            ? `당신은 ${currentMove}번째 순서에 있습니다…`
+            : "Game Start"}
+        </div>
       </div>
     </div>
   );
